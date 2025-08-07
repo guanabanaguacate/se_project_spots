80f3eda951626c5cdab606f00097a99d5b92164b
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2025-08-06 18:08:13 -0700

    stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,46 @@ newPostBtn.addEventListener("click", function(){
 newPostCloseBtn.addEventListener("click", function(){
     newPostModal.classList.remove("modal_is-opened")
 });
+
+
+const profileNameElement = document.querySelector("profile_name");
+const profileJobElement = document.querySelector("profile_description");
+
+const profileFormElement = editProfileModal.querySelector(."modal__form")
+const nameInput = editProfileModal.querySelector(".popup__input_name");
+const jobInput = editProfileModal.querySelector(".popup__input_description");
+ 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); 
+  inputName.value = profileNameElement.textContent;
+  inputJob.value = profileJobElement.textContent;
+});
+ 
+function fillInputFields(){
+    profileNameElement.textContent = nameInput.value;
+    profileJobElement.textContent = jobInput.value; 
+});
+
+  // Close the modal.
+
+// Set the submit listener.
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+// Select the necessary form elements. You should select
+// these from inside the modal, not the document.
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const linkInput = newPostModal.querySelector(".modal__label");
+const nameInput = newPostModal.querySelector(".modal__submit-btn");
+
+// Create the form submission handler.
+function handleAddCardSubmit(evt) {
+  evt.preventDefault(); 
+  console.log("linkInput.value");
+  console.log("nameinput.value");
+
+  // Close the modal.
+}
+
+// Create the submit listener.
+addCardFormElement.addEventListener('submit', handleAddCardSubmit);
