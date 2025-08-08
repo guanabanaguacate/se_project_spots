dcb6745cbf0dcbb5221ad20d0a7cea230d673dd2
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2025-08-08 09:09:54 -0700

    Stage 5 modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function(){
+    nameInput.value = profileNameElement.textContent;
+    jobInput.value = profileJobElement.textContent;
     editProfileModal.classList.add("modal_is-opened"); 
 }); 
 
@@ -23,45 +25,38 @@ newPostCloseBtn.addEventListener("click", function(){
     newPostModal.classList.remove("modal_is-opened")
 });
 
+const profileNameElement = document.querySelector(".profile_name");
+const profileJobElement = document.querySelector(".profile_description");
 
-const profileNameElement = document.querySelector("profile_name");
-const profileJobElement = document.querySelector("profile_description");
-
-const profileFormElement = editProfileModal.querySelector(."modal__form")
-const nameInput = editProfileModal.querySelector(".popup__input_name");
-const jobInput = editProfileModal.querySelector(".popup__input_description");
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const nameInput = editProfileModal.querySelector("#profile-name-input");
+const jobInput = editProfileModal.querySelector("#profile-description-input");
  
 function handleProfileFormSubmit(evt) {
   evt.preventDefault(); 
-  inputName.value = profileNameElement.textContent;
-  inputJob.value = profileJobElement.textContent;
-});
+  fillInputFields();
+  editProfileModal.classList.remove("modal_is-opened")
+};
  
 function fillInputFields(){
     profileNameElement.textContent = nameInput.value;
     profileJobElement.textContent = jobInput.value; 
-});
+};
 
   // Close the modal.
 
-// Set the submit listener.
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 
-
-// Select the necessary form elements. You should select
-// these from inside the modal, not the document.
 const addCardFormElement = newPostModal.querySelector(".modal__form");
-const linkInput = newPostModal.querySelector(".modal__label");
-const nameInput = newPostModal.querySelector(".modal__submit-btn");
+const linkInput = newPostModal.querySelector("#profile-name-input");
+const descriptionInput = newPostModal.querySelector("#profile-description-input");
 
-// Create the form submission handler.
 function handleAddCardSubmit(evt) {
   evt.preventDefault(); 
-  console.log("linkInput.value");
-  console.log("nameinput.value");
+  console.log(linkInput.value);
+  console.log(descriptionInput.value);
 
   // Close the modal.
 }
 
-// Create the submit listener.
 addCardFormElement.addEventListener('submit', handleAddCardSubmit);
