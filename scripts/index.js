e6bfb787efa07bda349b60e862308801266b9e6e
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2025-08-19 16:24:36 -0700

    create and loop an array for cards

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,16 @@ const addCardFormElement = newPostModal.querySelector(".modal__form");
 const linkInput = newPostModal.querySelector("#new-post-link");
 const descriptionInput = newPostModal.querySelector("#new-post-caption");
 
+const initialCards = [
+{ name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" }
+{ name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" }
+{ name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" }
+{ name: "A very long bridge, over the forest and through the trees", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg" }
+{ name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" }
+{ name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" }
+]
+
+
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -61,4 +71,9 @@ function handleAddCardSubmit(evt) {
   evt.target.reset(); 
 }
 
-addCardFormElement.addEventListener('submit', handleAddCardSubmit);
+addCardFormElement.addEventListener('submit', handleAddCardSubmit); 
+
+
+initialCards.forEach(function(card){
+  console.log(card.name);
+}); 
