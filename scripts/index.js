b8b135af895029cbf4e6bf9c7d07aeddf70642d0
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2025-09-09 14:34:09 -0700

    final project 1st pass

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,6 @@ const initialCards = [
 { name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" }
 ]
 
-
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -71,9 +70,85 @@ function handleAddCardSubmit(evt) {
   evt.target.reset(); 
 }
 
-addCardFormElement.addEventListener('submit', handleAddCardSubmit); 
+addCardFormElement.addEventListener('submit', function(evt){
+evt.preventDefault();
 
 
-initialCards.forEach(function(card){
-  console.log(card.name);
+
+}
+
+// this variable is needed right? 
+  const cardList = document.querySelector('.cards__list');
+
+initialCards.forEach(function(item){
+  const cardElement = getCardElement(item));
+  //prepend the created card element to the appropriate HTML container (the one where the hardcoed cards were located)
+  cardList.prepend(card);
 }); 
+
+//select template by it's id 
+const cardTemplate = document
+.querySelector("#card-Template") //.textContent; 
+
+//create function
+//why is "data" passed through instead of "item" ?
+//clone the content of the template
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+  .querySelector(".card")
+  .cloneNode(true);
+
+//select the clone's title and image elements and store them in variables
+const cardImageEl = cardElement.querySelector(".card__image"); 
+const cardTitleEl = cardElement.querySelector(".card__title");
+
+//Assign the data parameter’s link property to the image element’s src property.
+imageElement.src = data.link;
+
+// Assign the data parameter’s name property to the image element’s alt property.
+imageElement.alt = data.name;
+
+// Assign the data parameter’s name property to the name element’s textContent property.
+nameElement.textContent = data.name;
+
+//return the cloned card element
+return getCardElement;
+}
+
+//"NEW POST" MODAL SUBMISSION
+
+//these have already been called...
+//const nameInput = document.querySelector('.profile-name-input');
+//const descriptionInput = document.querySelector('.profile-description-input');
+
+const cardName = nameInput.value;
+//const linkInput = linkInput.value;
+
+//create a data object with the form values
+
+const newCardData = {
+  name: cardName,
+  link: cardLink
+}; 
+
+//create and add the card
+
+const newCardElement = getCardElement(newCardData); 
+
+//add it as the first element in the container
+
+cardContainer.prepend(newCardElement); 
+
+//when the user clicks on the card’s heart-shaped “like button,” the heart's color should change.   
+  
+  // 1. Select the card element's like button
+  //why is the cardElement being selected?
+
+  const likeButton = cardElement.querySelector(".card__like-button")
+
+  // 2. Set a click listener on it
+  //.classList.toggle ??
+
+  likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle('card__like-button_is-active');
+  })
